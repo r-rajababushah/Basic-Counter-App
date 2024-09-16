ed400011414343ec0293e505617a3e5e4c2a168d
Nikhil Patel <nikhilp9956@gmail.com>
2024-09-16 21:51:24 +0530

    Many Things fixed

--- src/App.js
+++ src/App.js
@@ -1,54 +1,20 @@
-import { Link, Outlet } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Home/Home";
+import Hotels from "./Hotels/Hotels";
+import Layout from "./Layout/Layout";
+import Resturants from "./Resturants/Resturants";
 
-function App() {
+export default function App() {
     return (
-        <>
-            <div className="mycls">
-
-                {/* First Header  */}
-                <div className="logoS">
-                    <a href="/"><img src="#" alt="logo" /></a>
-                    <div className="fMenu" >
-                        <ul className="adminD">
-                            <li><a href="#">Language </a></li>
-                            <li><a href="#">Sign Up </a></li>
-                            <li><a href="#">Log In </a></li>
-                        </ul>
-                    </div>
-                </div>
-
-                {/* Second Header */}
-                <div className="menuM">
-                    <div className="menuI">
-                        <span id="menuIcon">
-
-                        </span>
-                        <span id="homeB">
-                            <p><Link to="">Home</Link></p>
-                            <p>Search</p>
-                            <p><Link to="/hotels">Hotels</Link></p>
-                            <p><Link to="/resturnats">Resturants</Link></p>
-                        </span>
-                    </div>
-
-                    <div className="menuS">
-                        <span id="underratedP">
-                            <p>&#8595; &nbsp; Underrated Places </p>
-                        </span>
-                        <span id="emergencyN">
-                            <p>&#8595; &nbsp; Emergency Number </p>
-                        </span>
-                    </div>
-                </div>
-
-                <div className='homeMain'>
-                    <Outlet />
-                </div>
-
-            </div>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path='/hotels' element={<Hotels />} />
+                    <Route path='/resturants' element={<Resturants />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
-import Hotels from './Hotels';
-import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<App />}>
-					<Route index element={<App />} />
-					<Route path='/hotels' element={<Hotels />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<App />
 	</React.StrictMode>
 );
