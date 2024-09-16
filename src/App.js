4bb4db56f527f1f29a2a3f0111f8cc8a02995aa4
Rajababu Shah <rajababushah.in@gmail.com>
2024-09-16 06:15:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
 
 function App() {
-	const counter = useSelector((state) => state.counter);
-	const dispatch = useDispatch()
-
-	const increment = () => {
-		dispatch({ type: 'INCREMENT' });
-	}
-
-	const decrement = () => {
-		dispatch({ type: 'DECREMENT' });
-	}
-
 	return (
-		<div>
-			<h1>Counter</h1>
-			<h2>{counter}</h2>
-			<button onClick={increment}>Increment</button>
-			<button onClick={decrement}>Decrement</button>
-		</div>
+		<>
+		Hi
+		</>
 	);
 }
 
